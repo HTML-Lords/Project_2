ec285eea81f5c53cfd9b83e05e53eeb3f2620664
MalarkeyKazoo <6891850+malarkeykazoo@users.noreply.github.com>
2021-04-13 08:22:18 -0500

    clean up formatting

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-function init(){
+function init() {
   //Add listeners for different events
   window.addEventListener('load', init);
-  //JS Detection 
-  var html = document.getElementsByTagName("html")[0];
-    if(html.className == "nojs") {
-      html.className = html.className.replace("nojs", "hasjs");
-    }
+  //JS Detection
+  var html = document.getElementsByTagName('html')[0];
+  if (html.className == 'nojs') {
+    html.className = html.className.replace('nojs', 'hasjs');
+  }
 }
-$(document).ready(function(){
+$(document).ready(function () {
   $('.slider').bxSlider({
-  slideWidth: 600,
-  adaptiveHeight: true,
-  shrinkItems: true,
-  controls: true,
-  autoControls: false,
-  auto: true,
-  autoStart: true,
-  autoHover: false,
-  captions: false,
-  touchEnabled: true,
-  responsive: true,
-  pager:false,
-  mode: 'fade',
+    slideWidth: 600,
+    adaptiveHeight: true,
+    shrinkItems: true,
+    controls: true,
+    autoControls: false,
+    auto: true,
+    autoStart: true,
+    autoHover: false,
+    captions: false,
+    touchEnabled: true,
+    responsive: true,
+    pager: false,
+    mode: 'fade',
   });
 });
