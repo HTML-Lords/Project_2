3757eaabec6db2ed7f82af0847c96cd4c3012d07
MalarkeyKazoo <6891850+malarkeykazoo@users.noreply.github.com>
2021-04-13 19:26:01 -0500

    Tweaks to form scaling

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ function init() {
   if (html.className == 'nojs') {
     html.className = html.className.replace('nojs', 'hasjs');
   }
+  document.getElementById("newSubmit").addEventListener("click", newSub);
+  document.getElementById("contactForm").addEventListener("submit", showThanks);
+  function showThanks(){
+    event.preventDefault();
+    var formDisp = document.getElementById("contactForm");
+    var thanksDisp = document.getElementById("thanksPage");
+    var subCheck = document.getElementById("acceptTC");
+    if (subCheck.checked == true){  
+      thanksDisp.style.display = "block";
+      formDisp.style.display = "none";
+    }
+    else {
+      alert("You must agree to be contacted or we won't be able to contact you."); 
+    }
+  }
+
+  //Clears the form and show it again (hiding the thank you) for multiple submissions.
+  function newSub(){
+    var formDisp = document.getElementById("contactForm");
+    var thanksDisp = document.getElementById("thanksPage");
+    document.getElementById("contactForm").reset();
+    thanksDisp.style.display = "none";
+    formDisp.style.display = "block";
+  }
 }
 $(document).ready(function () {
   $('.slider').bxSlider({
